6d86bea7fccf2b718e3844723da1b85210d91b56
Demiat <demiat@mail.ru>
2026-05-02 14:51:47 +0300

    fix settings

--- server/core/settings.py
+++ server/core/settings.py
@@ -1,4 +1,5 @@
 from typing import Literal
+# from functools import cached_property
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -17,9 +18,12 @@ class DBSettings(BaseSettings):
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf8", extra="ignore")
-
-    @property
-    def db_url(self):
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.db_url = self._build_db_url()
+    
+    def _build_db_url(self) -> str:
         """
         Возвращает строку подключения к базе
         в зависимости от рабочего окружения.
@@ -27,13 +31,30 @@ class DBSettings(BaseSettings):
         if self.env == "test":
             return "sqlite+aiosqlite:///:memory:"
         elif self.env == "dev":
-            return f"sqlite+aiosqlite:///dev.db"
-        else:  # env prod
+            return "sqlite+aiosqlite:///dev.db"
+        else:  # prod
             return (
                 f"postgresql+asyncpg://{self.db_user}:"
                 f"{self.db_password}@"
                 f"{self.db_host}:{self.db_port}/{self.db_name}"
             )
+
+    # @cached_property
+    # def db_url(self):
+    #     """
+    #     Возвращает строку подключения к базе
+    #     в зависимости от рабочего окружения.
+    #     """
+    #     if self.env == "test":
+    #         return "sqlite+aiosqlite:///:memory:"
+    #     elif self.env == "dev":
+    #         return "sqlite+aiosqlite:///dev.db"
+    #     else:  # prod
+    #         return (
+    #             f"postgresql+asyncpg://{self.db_user}:"
+    #             f"{self.db_password}@"
+    #             f"{self.db_host}:{self.db_port}/{self.db_name}"
+    #         )
 
 
 class Settings(BaseSettings):
